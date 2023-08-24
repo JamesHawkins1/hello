15bd5e06060c28f0739032a99af6f4131d8a6260
JamesHawkins1 <jhawkins1@huskers.unl.edu>
2023-08-24 12:15:49 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,8 +1,17 @@
+/**
+ * Author: James Hawkins
+ * Date: 2023/08/24
+ *
+ * A simple program that prints name and majore
+ *
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    printf("James Hawkins");
+    printf("James Hawkins!\n");
+    printf("Computer Science");
 
     return 0;
 }
